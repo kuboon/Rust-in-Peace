bb0b9c1d942e45f20fb65e2dcd9358c9f7bf6c23
fumumue <fumumue@gmail.com>
2021-10-20 08:46:25 +0900

    const

--- ben-or/src/ben-or.c
+++ ben-or/src/ben-or.c
@@ -15,7 +15,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <assert.h>
+//#include <assert.h>
 #include <execinfo.h>
 
 //#include "8192.h"
@@ -25,10 +25,19 @@
 #include "debug.c"
 #include "chash.c"
 
+//nomal bases
+//unsigned short gf[M]={0,1,2,4,8,9,11,15,7,14,5,10,13,3,6,12};
+//unsigned short fg[M]={0,1,2,13,3,10,14,8,4,5,11,6,15,12,9,7};
 
+//sage比較用
+//static const unsigned short gf[16]={0,1,2,4,8,3,6,12,11,5,10,7,14,15,13,9};
+//static const unsigned short fg[16]={0,1,2,5,3,9,6,11,4,15,10,8,7,14,12,13};
+
+static const unsigned short gf[N] = {0, 1, 2, 4, 8, 16, 32, 64, 128, 29, 58, 116, 232, 205, 135, 19, 38, 76, 152, 45, 90, 180, 117, 234, 201, 143, 3, 6, 12, 24, 48, 96, 192, 157, 39, 78, 156, 37, 74, 148, 53, 106, 212, 181, 119, 238, 193, 159, 35, 70, 140, 5, 10, 20, 40, 80, 160, 93, 186, 105, 210, 185, 111, 222, 161, 95, 190, 97, 194, 153, 47, 94, 188, 101, 202, 137, 15, 30, 60, 120, 240, 253, 231, 211, 187, 107, 214, 177, 127, 254, 225, 223, 163, 91, 182, 113, 226, 217, 175, 67, 134, 17, 34, 68, 136, 13, 26, 52, 104, 208, 189, 103, 206, 129, 31, 62, 124, 248, 237, 199, 147, 59, 118, 236, 197, 151, 51, 102, 204, 133, 23, 46, 92, 184, 109, 218, 169, 79, 158, 33, 66, 132, 21, 42, 84, 168, 77, 154, 41, 82, 164, 85, 170, 73, 146, 57, 114, 228, 213, 183, 115, 230, 209, 191, 99, 198, 145, 63, 126, 252, 229, 215, 179, 123, 246, 241, 255, 227, 219, 171, 75, 150, 49, 98, 196, 149, 55, 110, 220, 165, 87, 174, 65, 130, 25, 50, 100, 200, 141, 7, 14, 28, 56, 112, 224, 221, 167, 83, 166, 81, 162, 89, 178, 121, 242, 249, 239, 195, 155, 43, 86, 172, 69, 138, 9, 18, 36, 72, 144, 61, 122, 244, 245, 247, 243, 251, 235, 203, 139, 11, 22, 44, 88, 176, 125, 250, 233, 207, 131, 27, 54, 108, 216, 173, 71, 142};
+static const unsigned short fg[N] = {0, 1, 2, 26, 3, 51, 27, 199, 4, 224, 52, 239, 28, 105, 200, 76, 5, 101, 225, 15, 53, 142, 240, 130, 29, 194, 106, 249, 201, 9, 77, 114, 6, 139, 102, 48, 226, 37, 16, 34, 54, 148, 143, 219, 241, 19, 131, 70, 30, 182, 195, 126, 107, 40, 250, 186, 202, 155, 10, 121, 78, 229, 115, 167, 7, 192, 140, 99, 103, 222, 49, 254, 227, 153, 38, 180, 17, 146, 35, 137, 55, 209, 149, 207, 144, 151, 220, 190, 242, 211, 20, 93, 132, 57, 71, 65, 31, 67, 183, 164, 196, 73, 127, 111, 108, 59, 41, 85, 251, 134, 187, 62, 203, 95, 156, 160, 11, 22, 122, 44, 79, 213, 230, 173, 116, 244, 168, 88, 8, 113, 193, 248, 141, 129, 100, 14, 104, 75, 223, 238, 50, 198, 255, 25, 228, 166, 154, 120, 39, 185, 181, 125, 18, 69, 147, 218, 36, 33, 138, 47, 56, 64, 210, 92, 150, 189, 208, 206, 145, 136, 152, 179, 221, 253, 191, 98, 243, 87, 212, 172, 21, 43, 94, 159, 133, 61, 58, 84, 72, 110, 66, 163, 32, 46, 68, 217, 184, 124, 165, 119, 197, 24, 74, 237, 128, 13, 112, 247, 109, 162, 60, 83, 42, 158, 86, 171, 252, 97, 135, 178, 188, 205, 63, 91, 204, 90, 96, 177, 157, 170, 161, 82, 12, 246, 23, 236, 123, 118, 45, 216, 80, 175, 214, 234, 231, 232, 174, 233, 117, 215, 245, 235, 169, 81, 89, 176};
 
 //Goppa多項式
-static unsigned short g[K + 1] = {1, 0, 0, 0, 1, 0, 1};
+static unsigned short g[K + 1] = {0};
 //{1,0,1,1};
 
 //有限体の元の逆数
@@ -122,7 +131,6 @@ void wait(void)
 {
   printf(" (enter number and hit return) "); // 何か表示させたほうが良いだろう
   fflush(stdout);                            // just in case
-  //scanf("%d", &a);                           // fgets(line, LINESIZE, stdin); という手も
   getchar();
 }
 
@@ -208,6 +216,7 @@ void oprintpol(OP f)
   }
 }
 
+/*
 void op_print_raw(const OP f)
 {
   puts("op_print_raw:");
@@ -249,15 +258,16 @@ bool op_verify(const OP f)
   }
   return true;
 }
+*/
 
 //20200816:正規化したいところだがうまく行かない
 //多項式の足し算
 OP oadd(OP f, OP g)
 {
-  f = conv(f);
-  g = conv(g);
-  assert(op_verify(f));
-  assert(op_verify(g));
+  //f = conv(f);
+  //g = conv(g);
+  ////assert(op_verify(f));
+  ////assert(op_verify(g));
 
   vec a = {0}, b = {0}, c = {0};
   int i;
@@ -275,7 +285,7 @@ OP oadd(OP f, OP g)
   }
   h = v2o(c);
   //h=conv(h);
-  assert(op_verify(h));
+  ////assert(op_verify(h));
   return h;
 }
 
@@ -339,7 +349,7 @@ oterm oLT(OP f)
 OP oterml(OP f, oterm t)
 {
   f = conv(f);
-  assert(op_verify(f));
+  ////assert(op_verify(f));
   int i;
   OP h = {0};
 
@@ -353,7 +363,7 @@ OP oterml(OP f, oterm t)
   }
 
   h = conv(h);
-  assert(op_verify(h));
+  //assert(op_verify(h));
   return h;
 }
 
@@ -362,8 +372,8 @@ OP omul(OP f, OP g)
 {
   f = conv(f);
   g = conv(g);
-  assert(op_verify(f));
-  assert(op_verify(g));
+  //assert(op_verify(f));
+  //assert(op_verify(g));
   int i, k, l;
   oterm t = {0};
   OP h = {0}, e = {0};
@@ -381,7 +391,7 @@ OP omul(OP f, OP g)
     e = oterml(f, t);
     h = oadd(h, e);
   }
-  assert(op_verify(h));
+  //assert(op_verify(h));
   return h;
 }
 
@@ -479,8 +489,8 @@ OP odiv(OP f, OP g)
 
   f = conv(f);
   g = conv(g);
-  assert(op_verify(f));
-  assert(op_verify(g));
+  //assert(op_verify(f));
+  //assert(op_verify(g));
   int i;
   OP h = {0}, tt = {0};
   oterm b = {0}, c = {0};
@@ -517,7 +527,7 @@ OP odiv(OP f, OP g)
   while (LT(f).n > 0 && LT(g).n > 0)
   {
     c = LTdiv(f, b);
-    assert(c.n < DEG);
+    //assert(c.n < DEG);
     tt.t[i] = c;
     i++;
 
@@ -542,7 +552,7 @@ OP odiv(OP f, OP g)
     ret.t[i] = tt.t[tt_terms - i - 1];
   }
   ret = conv(ret);
-  assert(op_verify(ret));
+  //assert(op_verify(ret));
   return ret;
 }
 
@@ -566,26 +576,15 @@ OP omod(OP f, OP g)
   OP h = {0};
   oterm b = {0}, c = {0};
 
-  //n = LT(g).n;
-
-  //  assert (("baka^\n", LT (f).n != 0));
-
-  //  assert (("baka(A)\n", LT (g).n != 0));
-
   if (LT(f).n < LT(g).n)
   {
     //    exit(1);
     return f;
   }
 
-  //printf ("in omod\n");
-  //exit(1);
 
   b = LT(g);
-  //printpol(o2v(g));
-  //printf("d %d\n",LT(g).n);
 
-  //assert(("double baka\n", b.a != 0 && b.n != 0));
   while (LT(f).n > 0 && LT(g).n > 0)
   {
 
@@ -594,7 +593,6 @@ OP omod(OP f, OP g)
     f = oadd(f, h);
     if (odeg((f)) == 0 || odeg((g)) == 0)
     {
-      //      printf("blake1\n");
       break;
     }
 
@@ -660,7 +658,11 @@ void printsage(vec a)
       j = v2a(b);
       //printf("%d,==ba\n",b.a);
       //printf ("X**%d+", i); //for GF2
+      if(i==K){
+      printf("B('a^%d')*X**%d;", j, i); //for GF(2^m)
+      }else{
       printf("B('a^%d')*X**%d+", j, i); //for GF(2^m)
+      }
     }
   }
 }
@@ -990,8 +992,6 @@ aa:
     //
   }
 
-  //w = mkpol();
-
   //多項式の値が0でないことを確認
   for (i = 0; i < N; i++)
   {
@@ -1007,7 +1007,7 @@ aa:
   printpol(o2v(w));
   printf(" =irreducible\n");
   printsage(o2v(w));
-  printf(" print(poly.is_irreducible())\n");
+  printf(" print(poly.is_irreducible());\n");
   j++;
 
   if (j > 100)
@@ -1018,7 +1018,9 @@ aa:
 //言わずもがな
 int main(void)
 {
+  time_t t;
 
+  srand(clock() * time(&t));
   chu();
 
   return 0;
