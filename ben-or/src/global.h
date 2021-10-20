006466eeaf8819d42de67ff8f3d43aba38d4f9de
fumumue <fumumue@gmail.com>
2021-10-20 13:24:39 +0900

    modified

--- ben-or/src/global.h
+++ ben-or/src/global.h
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "8192.h"
 
 //符号のパラーメータの指定。通常[N,K,T]として、
 //Nは符号の長さ、Kが符号の次元、Tは訂正エラー数
 //を表す。ここではDは符号長にしている。
-#define N 256  // 符号長
-#define M 256 //有限体の元の数
-#define K (16) //符号の次元
-#define E (8)    //拡大体のビット数
-#define DEG N+1 //(K * E)
+#define N 8192  // 符号長
+#define M 8192 //有限体の元の数
+#define K (64) //符号の次元
+#define E (13)    //拡大体のビット数
+#define DEG (K * E) //多項式の次数
 #define T (K / 2) //エラーの数
 
 
